998a46c78a17940247bd000dd4be976e8ca2efff
alumno <alumno@aula01-02>
2016-10-24 19:46:27 +0200

    Ejercicio terminado

--- NumerosPrimos/src/numerosprimos/NumerosPrimos.java
+++ NumerosPrimos/src/numerosprimos/NumerosPrimos.java
@@ -1,21 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package numerosprimos;
 
-/**
- *
- * @author alumno
- */
-public class NumerosPrimos {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class NumerosPrimos {
+    
+    public static int SiguientePrimo(int num){
+        
+        int primo = 0;
+        boolean esPrimo = true;
+        
+        for(int i = (num+1);esPrimo == true;i++){
+            if(ComprobarPrimo(i)){
+                esPrimo = false;
+                primo = i;
+            }
+        }
+        
+        return primo;
+    }
+    
+    //Devuelve true si el número es primo, false en caso contrario.
+    public static boolean ComprobarPrimo(int num){
+        
+        boolean primo = true;
+        
+        for(int i=2;i<num;i++){
+            if(num % i == 0){
+               primo = false;
+               i = num + 1;
+            }
+        }       
+        return primo;
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner leer = new Scanner(System.in);
+        int numero = 0;
+        boolean bandera = true;
+        
+        while(bandera){
+            
+            System.out.println("Introduzca un número");
+            numero = leer.nextInt();
+            
+            if(numero != 0){
+                
+                if(ComprobarPrimo(numero)){
+                System.out.println("El número " + numero + " es primo");
+                }else{
+                System.out.println("El número " + numero +" no es primo");
+                }
+            
+                System.out.println("El siguiente número primo es: " + SiguientePrimo(numero));
+                System.out.println("");
+                
+            }else{
+                
+                bandera = false;
+            }
+        }
     }
     
 }
